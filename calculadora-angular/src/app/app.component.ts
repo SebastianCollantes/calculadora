4487173364f757eda87c4e74d366651f4b0e6e13
Sebastian <admin@MacBook-Pro-de-Usuario.local>
2023-06-25 19:07:31 -0400

    se puede sumar

--- calculadora-angular/src/app/app.component.ts
+++ calculadora-angular/src/app/app.component.ts
@@ -1,10 +1,48 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'calculadora-angular';
+export class AppComponent implements OnInit{
+
+  actualNumber: number;
+  result: number;
+
+  constructor(){
+    this.actualNumber = 0;
+    this.result = 0;
+  }
+
+  ngOnInit(): void {
+  }
+  addNumber(numero: number){
+    this.actualNumber = this.actualNumber * 10 + numero;
+    this.refreshResult();
+  }
+  plusNumber(){
+    this.result += this.actualNumber;
+    this.actualNumber = 0;
+    this.refreshResult();
+  }
+  minusNumber(){
+    
+  }
+  deleteInput(){
+    this.actualNumber = 0;
+    this.result = 0;
+    this.refreshResult();
+  }
+  calculate(){
+    this.result += this.actualNumber;
+    this.actualNumber = this.result;
+    this.refreshResult();
+  }
+  refreshResult(){
+    var inputField = document.getElementById('inputField');
+    if(inputField instanceof HTMLInputElement){
+      inputField.value = this.actualNumber.toString();
+    }
+  }
 }
