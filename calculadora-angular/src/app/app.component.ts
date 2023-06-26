804a9d8c335195f6dcec9811530880fd17a1ab79
Sebastian <admin@MacBook-Pro-de-Usuario.local>
2023-06-26 14:34:07 -0400

    se agregaron todas las funciones de la calculadora, excepto el . y el +/-

--- calculadora-angular/src/app/app.component.ts
+++ calculadora-angular/src/app/app.component.ts
@@ -5,43 +5,65 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements OnInit{
+export class AppComponent implements OnInit {
 
   actualNumber: number;
   result: number;
+  operator: string;
 
-  constructor(){
+  constructor() {
     this.actualNumber = 0;
     this.result = 0;
+    this.operator = '';
   }
 
   ngOnInit(): void {
   }
-  addNumber(numero: number){
+
+  addNumber(numero: number) {
     this.actualNumber = this.actualNumber * 10 + numero;
     this.refreshResult();
   }
-  plusNumber(){
-    this.result += this.actualNumber;
+
+  setOperator(operator: string) {
+    this.operator = operator;
+    this.result = this.actualNumber;
     this.actualNumber = 0;
     this.refreshResult();
   }
-  minusNumber(){
-    
-  }
-  deleteInput(){
-    this.actualNumber = 0;
-    this.result = 0;
-    this.refreshResult();
-  }
-  calculate(){
-    this.result += this.actualNumber;
+
+  calculate() {
+    if (this.operator === '+') {
+      this.result += this.actualNumber;
+    } else if (this.operator === '-') {
+      this.result -= this.actualNumber;
+    } else if (this.operator === '*') {
+      this.result *= this.actualNumber;
+    } else if (this.operator === '/') {
+      if (this.actualNumber === 0) {
+        this.result = NaN;
+      } else {
+        this.result /= this.actualNumber;
+      }
+    } else if(this.operator === '%'){
+      this.result = this.result * (this.actualNumber / 100);
+    }
+
+    this.operator = '';
     this.actualNumber = this.result;
     this.refreshResult();
   }
-  refreshResult(){
+
+  deleteInput() {
+    this.actualNumber = 0;
+    this.result = 0;
+    this.operator = '';
+    this.refreshResult();
+  }
+
+  refreshResult() {
     var inputField = document.getElementById('inputField');
-    if(inputField instanceof HTMLInputElement){
+    if (inputField instanceof HTMLInputElement) {
       inputField.value = this.actualNumber.toString();
     }
   }
